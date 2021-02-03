ca75bf7023611498fafa9de570affa1f741da4c5
Rich King <richardrking@pm.me>
2021-02-03 00:24:34 -0800

    Created a basic <Card> component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dev portfolio :)
-        </p>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Card() {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p>Project title</p>
+        </div>
+    );
+}
+
+export default Card;
+
