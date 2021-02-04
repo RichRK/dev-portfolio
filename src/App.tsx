ed55736b7d695da5122f4c4296269c942146c831
Rich King <richardrking+github@pm.me>
2021-02-03 23:47:31 -0800

    Fleshing out content

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Card from './Card'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-lg text-gray-50 flex flex-col justify-center items-center">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+    <div className="min-h-screen bg-gray-900 text-lg text-gray-50 flex flex-col justify-center items-center pt-4">
+      <h1 className="font-bold text-5xl">Dev work</h1>
+      <p>This is a collection of projects that I've worked on. Some big, some small — but all valuable experiences.</p>
+      <Card title="Listing form" />
+      <Card title="PID generator" />
+      <Card title="Automated testing" />
+      <Card title="Fetching data from Microsoft Dynamics" />
+      <Card title="'Community Maps'" />
+      <Card title="'Remember When' site" />
     </div>
   );
 }
