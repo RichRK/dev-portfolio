f8a27b8f06b6fb7e5215652cbc72253a5263ad2a
Rich King <richardrking+github@pm.me>
2021-02-03 23:48:04 -0800

    Gave each <Card> a title, minor styling changes

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 
-function Card() {
-    return (
-        <div>
-            <img src={logo} alt="" />
-            <p>Project title</p>
-        </div>
-    );
+function Card(props: any){
+  return (
+    <div className="border border-gray-600 mb-4">
+      <img src={logo} alt="" />
+      <p>{props.title}</p>
+    </div>
+  );
 }
 
 export default Card;
 
+Card.defaultProps = {
+  
+  title: "Title"
+}
