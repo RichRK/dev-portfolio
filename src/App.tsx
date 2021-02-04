f353a8e7a83a91d88be73046b235f53112b705b5
Rich King <richardrking+github@pm.me>
2021-02-04 00:07:42 -0800

    Cards now generated and iterated over

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-900 text-lg text-gray-50 flex flex-col justify-center items-center pt-4">
-      <h1 className="font-bold text-5xl">Dev work</h1>
-      <p>This is a collection of projects that I've worked on. Some big, some small — but all valuable experiences.</p>
-      <Card title="Listing form" />
-      <Card title="PID generator" />
-      <Card title="Automated testing" />
-      <Card title="Fetching data from Microsoft Dynamics" />
-      <Card title="'Community Maps'" />
-      <Card title="'Remember When' site" />
-    </div>
+
+  const titles = [ "Listing form", "PID generator", "Automated testing", "Fetching data from Microsoft Dynamics", "'Community Maps'", "'Remember When' site" ];
+
+  const cards = titles.map( (title) =>
+    <Card key={title} title={title} />
   );
+
+  return (
+
+    <div className="min-h-screen bg-gray-900 text-lg text-gray-50 flex flex-col justify-center items-center pt-4">
+
+      <h1 className="font-bold text-5xl">Dev work</h1>
+
+      <p>This is a collection of projects that I've worked on. Some big, some small — but all valuable experiences.</p>
+
+      {cards}
+      
+    </div>
+
+  );
+
 }
 
 export default App;
