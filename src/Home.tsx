5d267eac0d0d2813745fdfc630f11e2d83be8784
Rich <richardrking+github@pm.me>
2021-02-06 22:44:51 -0800

    Added react router

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,7 +17,7 @@ function App() {
   );
 
   return (
-    <div className="grid grid-cols-1 gap-8 p-8 min-h-screen bg-gray-900 text-lg text-gray-100 sm:grid-cols-2 lg:grid-cols-3 xl:px-20 xl:py-14">
+    <div className="grid grid-cols-1 gap-8 p-8 sm:grid-cols-2 lg:grid-cols-3 xl:px-20 xl:py-14">
       <h1 className="font-bold text-5xl">Dev work</h1>
       <p className="col-start-1 col-span-full mb-4 sm:w-2/3 lg:w-1/2 xl:mb-6">A selection of projects I've worked on — some big and some small, all full of their own challenges.</p>
       { cards }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" component={ Home } />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -16,3 +19,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
