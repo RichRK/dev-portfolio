f505d19c9e8ec432926c066c92bfdae750736e8b
Rich <richardrking+github@pm.me>
2021-02-21 23:55:00 -0800

    Splitting blocks of text into paragraphs

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,10 +4,14 @@ function Page(props: any){
 
   const contentExists = Object.keys( props.content ).length;
 
-  const content = Object.entries( props.content ).map( ( [ key, text ]: any ) =>
-    <React.Fragment key={ key }>
-      <h2 className="font-bold text-2xl capitalize">{ key }</h2>
-      <p className="col-start-1 col-span-full mb-4 sm:w-2/3 lg:w-1/2 xl:mb-6">{ text }</p>
+  const content = Object.entries( props.content ).map( ( [ subheading, section ]: any ) =>
+    <React.Fragment key={ subheading }>
+      <h2 className="font-bold text-2xl capitalize">
+        { subheading }
+      </h2>
+      { section.map( ( paragraph: string ) =>
+        <p className="col-start-1 col-span-full mb-4 sm:w-2/3 lg:w-1/2 xl:mb-6">{ paragraph }</p>
+      ) }
     </React.Fragment>
   );
 
