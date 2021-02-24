7b8fdf98d383ee6db48555f13c1e49cb39f6648a
Rich King <richardrking+github@pm.me>
2021-02-24 01:01:45 -0800

    Custom Tailwind plugin to enable using ::selection

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       'sans': [ '"Josefin Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif' ]
-    },
+    }
   },
   variants: {
-    extend: {},
+    backgroundColor: ['selection'],
+    extend: {}
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant, e }) {
+      addVariant('selection', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`selection${separator}${className}`)}::selection`
+        })
+      })
+    })
+  ]
 }
 
