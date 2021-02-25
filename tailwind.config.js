300b39e2930e293acb9ac58343938858f8543a11
Rich King <richardrking+github@pm.me>
2021-02-24 20:25:12 -0800

    Better ::selection colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require( 'tailwindcss/plugin' )
+const colors = require( 'tailwindcss/colors' )
 
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        bluegray: colors.blueGray
+      }
+    },
     fontFamily: {
       'sans': [ '"Josefin Sans"', 'ui-sans-serif', 'system-ui', 'sans-serif' ]
     }
