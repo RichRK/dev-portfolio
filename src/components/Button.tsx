84250c3f7a8363a7d18a614f94a2ee89da605de9
Rich <richardrking+github@pm.me>
2021-02-28 13:52:49 -0800

    Adding focus behaviour

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ function Button(props: ButtonProps){
   return (
 
     <a className="w-max mb-3" href={ props.url } rel="noreferrer" target="_blank">
-      <button className="w-max rounded-md px-4 pt-2 pb-1 border-2 border-gray-700">
+      <button className="w-max rounded-md px-4 pt-2 pb-1 border-2 border-gray-700 focus:outline-none focus:ring-4 ring-gray-700">
         { props.children }
         { props.icon ?
           <img
